4d85507f93629d63366a93c902a27affda3aa7d0
unknown <jinyeob.kim.0818@gmail.com>
2017-11-29 08:49:31 -0600

    Working On ProgressiveManager

--- classes.js
+++ classes.js
@@ -22,7 +22,14 @@ Each employee can:
 call your class Employee and receive all the data in the constructor in the order listed
 */
 
-
+class Employee {
+    constructor (first_name,last_name,email,age) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.age = age;
+    }
+}
 
 /*
 
@@ -41,7 +48,21 @@ call your class Manager
 
 */
 
-
+class Manager {
+    constructor (first_name,last_name,email,age) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.age = age;
+        this.reports = [];
+    }
+    hire (employee) {
+        this.reports.push(employee);
+    }
+    fire (index) {
+        this.reports.splice(index,1);
+    }
+}
 
 
 
@@ -65,7 +86,33 @@ Everytime they fire an employee they get $100 added to their bonus.
 call your class ProgressiveManager
 */
 
-
+class ProgressiveManager {
+    constructor (first_name,last_name,email,age) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.age = age;
+        this.reports = [];
+        this.title = 'Not a manager';
+        this.bonus = 0;
+    }
+    employeeCheck (add,remove) {
+        this.reports.push(add);
+        if (this.reports.length===0) {
+            this.title = 'Not a manager';
+        } else if (this.reports.length>=1 && this.reports.length<=3) {
+            this.title = 'Barely Manager';
+        } else if (this.reports.length>=4 && this.reports.length<=10) {
+            this.title = 'Mostly Manager';
+        } else if (this.reports.length>=11 && this.reports.length<=50) {
+            this.title = 'Manager';
+        } else if (this.reports.length>=51 && this.reports.length<=100) {
+            this.title = 'Manager Plus';
+        } else if (this.reports.length>=101) {
+            this.title = 'Bestest Manager';
+        }
+    }
+}
 
 
 /*
